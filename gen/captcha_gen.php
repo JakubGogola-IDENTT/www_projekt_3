7f0e4810b81c344c49e81aef9f3060c23c028b34
JakubGogola <jakubgogola97@gmail.com>
2018-06-09 21:27:47 +0200

    Fixing bugs.

--- gen/captcha_gen.php
+++ gen/captcha_gen.php
@@ -63,7 +63,7 @@ class captcha_gen {
     private function det($matrix) {
         return $matrix[0][0] * $matrix[1][1] * $matrix[2][2] +
             $matrix[0][1] * $matrix[1][2] * $matrix[2][0] +
-            $matrix[0][2] * $matrix[1][0] * $matrix[1][2] -
+            $matrix[0][2] * $matrix[1][0] * $matrix[2][1] -
             $matrix[2][0] * $matrix[1][1] * $matrix[0][2] -
             $matrix[2][1] * $matrix[1][2] * $matrix[0][0] -
             $matrix[2][2] * $matrix[1][0] * $matrix[0][1];
